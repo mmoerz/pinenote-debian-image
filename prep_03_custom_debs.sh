7bf31062264ddf040cb39ac5fc4f7bdcfe380131
Michael Moerz <michael@natoka.de>
2025-02-06 14:20:20 +0100

    replaced superflous test with forced delete

--- prep_03_custom_debs.sh
+++ prep_03_custom_debs.sh
@@ -3,7 +3,7 @@ pwd=$PWD
 
 # koreader
 cd overlays/custom_debs/
-test -e koreader-2023.08-arm64.deb && rm koreader*.deb
+rm -f koreader*.deb
 # wget -nv https://github.com/koreader/koreader/releases/download/v2024.04/koreader-2024.04-arm64.deb
 # wget -nv https://github.com/koreader/koreader/releases/download/v2024.07/koreader-2024.07-arm64.deb
 wget -nv https://github.com/koreader/koreader/releases/download/v2024.11/koreader-2024.11-arm64.deb
@@ -11,6 +11,6 @@ cd ${pwd}
 
 # evsieve
 cd overlays/custom_debs/
-test -e evsieve_1.3.1_arm64.deb && rm evsieve*.deb
+rm -f evsieve*.deb
 wget https://github.com/m-weigand/evsieve_pn/releases/download/v1.3.1-arm64/evsieve_1.3.1_arm64.deb
 cd "${pwd}"
